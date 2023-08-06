b1373b19eb1f2adfa601e7c354797c082b1a9a83
Huntington Jasperson <huntingtonjasperson@gmail.com>
2023-08-06 16:56:46 -0700

    init node project. Added express and basic sankey diagram modules.

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World');
+// });
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+})
---
+++ public/sankey.js
@@ -0,0 +1,35 @@
+import 'plotly.js-dist-min';
+
+var data = {
+  type: "sankey",
+  orientation: "h",
+  node: {
+    pad: 15,
+    thickness: 30,
+    line: {
+      color: "black",
+      width: 0.5
+    },
+    label: ["A1", "A2", "B1", "B2", "C1", "C2"],
+    color: ["blue", "blue", "blue", "blue", "blue", "blue"]
+  },
+
+  link: {
+    source: [0, 1, 0, 2, 3, 3],
+    target: [2, 3, 3, 4, 4, 5],
+    value: [8, 4, 2, 8, 4, 2]
+  }
+}
+
+var data = [data]
+
+var layout = {
+  title: "Basic Sankey",
+  width: 1118,
+  height: 772,
+  font: {
+    size: 10
+  }
+}
+
+Plotly.react('harnessDiv', data, layout)

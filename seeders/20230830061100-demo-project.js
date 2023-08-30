186ef25d71d655366f7aea5ea575ee9a50f8b56a
Huntington Jasperson <huntingtonjasperson@gmail.com>
2023-08-30 03:48:56 -0700

    Configure db migrations and seeders. Created test migration and test seed.

---
+++ seeders/20230830061100-demo-project.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    return queryInterface.bulkInsert('Projects', [{
+      Name: 'CA18DET_ENGINE_HARNESS',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    return queryInterface.bulkDelete('Projects', null, {});
+  }
+};

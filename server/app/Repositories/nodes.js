d7e3661acf7a3505ab80be1d5eee7d4e72a0e018
Huntington Jasperson <huntingtonjasperson@gmail.com>
2023-09-13 16:20:49 -0700

    Added createNode and getLastNode methods

--- server/app/Repositories/nodes.js
+++ server/app/Repositories/nodes.js
@@ -11,6 +11,23 @@ class Nodes {
     );
     return results;
   }
+
+  async getLastNode(projectId) {
+    const [results, metadata] = await this.db.query(
+      `
+        SELECT * FROM Nodes WHERE projectId = ${projectId} ORDER BY projectIndex DESC LIMIT 1;
+      `
+    );
+  }
+
+  async createNode(projectId, projectIndex, label, color, type = "default") {
+    const [results, metadata] = await this.db.query(
+      `
+        INSERT INTO Nodes (projectId, projectIndex, label, color, type, createdAt, updatedAt) VALUES (${projectId}, ${projectIndex}, "${label}", "${color}", "${type}", now(), now());
+      `
+    );
+    return results;
+  }
 }
 
 module.exports = Nodes;
